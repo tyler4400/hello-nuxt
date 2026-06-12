5e4c584db42ec7641d45117a58f705994dd0f8f7
tyler4400 <tyler4400@foxmail.com>
2026-06-12 14:30:44 +0800

    21-1 添加tailwind 注意 @nuxtjs/tailwindcss 还是停留在支持nuxt3，新版要使用Tailwind 官网的方式（tailwindcss + @tailwindcss/vite）

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
@@ -56,4 +58,9 @@ export default defineNuxtConfig({
       // },
     },
   },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  }
 })
